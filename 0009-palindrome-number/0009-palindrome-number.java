466869245065b2f9857227beeb13c9a6fd33d4b0
Paritosh Upadhyay <paritoshup205@gmail.com>
2026-07-06 23:34:45 +0530

    Time: 5 ms (79.94%), Space: 46.2 MB (17.21%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+
+        int original = x;
+        int rev = 0;
+
+        while (x != 0) {
+            int ld = x % 10;
+            rev = rev * 10 + ld;
+            x /= 10;
+        }
+
+        return rev == original;
+    }
+}
